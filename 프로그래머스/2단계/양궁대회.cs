51d2dc7b66d36b1301fb9212d2b9f0ec23fa4c2c
ITJEONG-DEV <derbana1027@gmail.com>
2023-07-20 18:25:39 +0900

    (230720) 프로그래머스 문제 풀이

---
+++ 프로그래머스/2단계/양궁대회.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 프로그래머스.양궁대회
+{
+    public class Solution
+    {
+        public bool GetLowScoreDistribution(ref int[] a, ref int[] b)
+        {
+            for(int i=10; i>=0; i--)
+            {
+                if (a[i] > b[i])
+                    return true;
+            }
+
+            return false;
+        }
+        public int GetScoreDifference(ref int[] rion, ref int[] appeach)
+        {
+            int rScore = 0;
+            int aScore = 0;
+            for(int i=0; i<11; i++)
+            {
+                if (rion[i] <= appeach[i])
+                    aScore += 10 - i;
+                else
+                    rScore += 10 - i;
+            }
+
+            return rScore - aScore;
+        }
+        public int[] FindResult(int[] rion, int currentN, int n, int currentIndex, int maxIndex, ref int[] appeach)
+        {
+            if (currentN == n)
+            {
+                return rion;
+            }
+            if(currentIndex == -1)
+            {
+                rion[10] += n - currentN;
+                return rion;
+            }
+
+            int[] result1 = null;
+            int[] result2 = null;
+
+            int appeachScore = appeach[currentIndex];
+
+            if(currentN + appeachScore + 1 <= n)
+            {
+                int[] newRion = (int[])rion.Clone();
+                newRion[currentIndex] = appeachScore + 1;
+
+                int newN = currentN + appeachScore + 1;
+
+                result1 = FindResult(newRion, newN, n, currentIndex - 1, maxIndex, ref appeach);
+            }
+
+            result2 = FindResult(rion, currentN, n, currentIndex - 1, maxIndex, ref appeach);
+
+            if (result1 == null && result2 == null) return null;
+            else if (result1 == null) return result2;
+            else if (result2 == null) return result1;
+            else
+            {
+                int s1 = GetScoreDifference(ref result1, ref appeach);
+                int s2 = GetScoreDifference(ref result2, ref appeach);
+
+                if(s1 == s2)
+                    return GetLowScoreDistribution(ref result1, ref result2) ? result1: result2;
+                else return s1 > s2 ? result1 : result2;
+            }
+        }
+        public int[] solution(int n, int[] info)
+        {
+            int[] rion = new int[11];
+            var result = FindResult(rion, 0, n, 10, 10, ref info);
+
+            int scoreDifference = GetScoreDifference(ref result, ref info);
+
+            return scoreDifference <= 0 ? new int[] {-1} : result;
+        }
+    }
+}
---
+++ 프로그래머스/2단계/주차 요금 계산.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 프로그래머스.주차_요금_계산
+{
+    public class Solution
+    {
+        public class Car
+        {
+            public int Id { get; set; }
+            string enteringTime = null;
+            string exitingTime = null;
+            int ParkingTime = 0;
+
+            public Car(int id)
+            {
+                this.Id = id;
+            }
+
+            public void AddEnteringTime(string time)
+            {
+                this.enteringTime = time;
+            }
+            public void AddExitingTime(string time)
+            {
+                this.exitingTime = time;
+                OperateParkingTime();
+            }
+
+            public void OperateParkingTime()
+            {
+                if (this.enteringTime == null) return;
+
+                if (this.exitingTime == null)
+                    this.exitingTime = "23:59";
+
+                var entering = enteringTime.Split(':');
+                var exiting = exitingTime.Split(':');
+
+                int hourDiffer = int.Parse(exiting[0]) - int.Parse(entering[0]);
+                int minuteDiffer = int.Parse(exiting[1]) - int.Parse(entering[1]);
+
+                this.ParkingTime += hourDiffer * 60 + minuteDiffer;
+
+                this.enteringTime = null;
+                this.exitingTime = null;
+            }
+
+            public int GetParkingTime()
+            {
+                OperateParkingTime();
+                return this.ParkingTime;
+            }
+        }
+        public int[] solution(int[] fees, string[] records)
+        {
+            SortedDictionary<int, Car> carDict = new SortedDictionary<int, Car>();
+
+            for (int i = 0; i < records.Length; i++)
+            {
+                var words = records[i].Split();
+
+                var id = int.Parse(words[1]);
+
+                if (!carDict.ContainsKey(id))
+                    carDict[id] = new Car(id);
+
+                if (words[2] == "IN")
+                    carDict[id].AddEnteringTime(words[0]);
+                else
+                    carDict[id].AddExitingTime(words[0]);
+            }
+
+            List<int> feeList = new List<int>();
+            foreach (var key in carDict.Keys)
+            {
+                Car car = carDict[key];
+                int parkingTime = car.GetParkingTime();
+
+                if (parkingTime == 0) continue;
+
+                if (parkingTime <= fees[0])
+                {
+                    Console.WriteLine($"Car({car.Id}) Time({parkingTime}) Cost({fees[1]})");
+                    feeList.Add(fees[1]);
+                    continue;
+                }
+
+                parkingTime -= fees[0];
+
+                var cost = fees[1] + (int)Math.Ceiling((double)parkingTime / fees[2]) * fees[3];
+                feeList.Add(cost);
+                Console.WriteLine($"Car({car.Id}) Time({parkingTime + fees[0]}) Cost({cost})");
+            }
+
+            return feeList.ToArray();
+        }
+    }
+}
