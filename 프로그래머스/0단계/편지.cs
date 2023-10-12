cde138a730d5ad91b6cf7b7fc4eea29ba8506ef7
ITJEONG-DEV <derbana1027@gmail.com>
2023-10-12 10:55:10 +0900

    (231012) 프로그래머스 문제 풀이
    나머지 풀이한 문제들은 다른 레포지토리로 이관

---
+++ 프로그래머스/0단계/편지.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 프로그래머스.편지
+{
+    public class Solution
+    {
+        public int solution(string message)
+        {
+            return message.Length * 2;
+        }
+    }
+}
